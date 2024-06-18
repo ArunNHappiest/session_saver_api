9b74e7b4d009afd932115b0c21ef5bc1df1a5861
Arun Natarajan <arun.p.natarajan@happiestminds.com>
2024-06-18 18:14:51 +0530

    RedisSession handler

---
+++ demo_client/RedisSessionHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+class RedisSessionHandler implements SessionHandlerInterface
+{
+    private $redis;
+    private $ttl;
+
+    public function __construct($host, $port, $ttl = 3600)
+    {
+        $this->ttl = $ttl;
+
+        $this->redis = new Redis();
+        $this->redis->connect($host, $port);
+    }
+
+    public function open($savePath, $sessionName)
+    {
+        return true;
+    }
+
+    public function close()
+    {
+        return true;
+    }
+
+    public function read($sessionId)
+    {
+        $data = $this->redis->get($sessionId);
+        return $data ? $data : '';
+    }
+
+    public function write($sessionId, $data)
+    {
+        return $this->redis->setex($sessionId, $this->ttl, $data);
+    }
+
+    public function destroy($sessionId)
+    {
+        $this->redis->del($sessionId);
+        return true;
+    }
+
+    public function gc($maxLifetime)
+    {
+        // Redis handles expiration automatically, no action necessary
+        return true;
+    }
+}
--- demo_client/inc.php
+++ demo_client/inc.php
@@ -1,8 +1,15 @@
 <?php
 ob_start();
-require 'ApiSessionHandler.php';
+// require 'ApiSessionHandler.php';
 
-$apiBaseUrl = 'https://session.commonstatesaver.com/index.php';
-$handler = new ApiSessionHandler($apiBaseUrl);
+// $apiBaseUrl = 'https://session.commonstatesaver.com/index.php';
+// $handler = new ApiSessionHandler($apiBaseUrl);
+require 'RedisSessionHandler.php';
+
+$redisHost = 'clustercfg.pgsession2.pr3z0i.memorydb.ap-south-1.amazonaws.com';
+$redisPort = 6379;
+$ttl = 3600;
+$handler = new RedisSessionHandler($redisHost, $redisPort, $ttl);
+
 session_set_save_handler($handler, true);
 session_start();
