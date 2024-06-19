ffcc4b71974e87e4397cdf3557a775e4c6660404
Arun Natarajan <arun.p.natarajan@happiestminds.com>
2024-06-19 14:36:48 +0530

    redis RedisClusterSessionHandler

---
+++ demo_client/RedisClusterSessionHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+class RedisClusterSessionHandler implements SessionHandlerInterface {
+    private $redis;
+    private $ttl;
+
+    public function __construct(array $clusterNodes, $ttl = 3600) {
+        $this->ttl = $ttl;
+
+        // Create a RedisCluster instance
+        $this->redis = new RedisCluster(NULL, $clusterNodes);
+        if (!$this->redis) {
+            throw new Exception("Could not connect to Redis cluster.");
+        }
+    }
+
+    public function open($savePath, $sessionName) {
+        return true;
+    }
+
+    public function close() {
+        return true;
+    }
+
+    public function read($sessionId) {
+        $data = $this->redis->get($sessionId);
+        return $data ? $data : '';
+    }
+
+    public function write($sessionId, $data) {
+        return $this->redis->setex($sessionId, $this->ttl, $data);
+    }
+
+    public function destroy($sessionId) {
+        $this->redis->del($sessionId);
+        return true;
+    }
+
+    public function gc($maxLifetime) {
+        // Redis handles expiration automatically
+        return true;
+    }
+
+    public function create_sid() {
+        $sid = bin2hex(random_bytes(32)); // Create a secure session ID
+        return $sid;
+    }
+}
--- demo_client/inc.php
+++ demo_client/inc.php
@@ -1,15 +1,23 @@
 <?php
 ob_start();
-// require 'ApiSessionHandler.php';
+// require_once 'ApiSessionHandler.php';
 
 // $apiBaseUrl = 'https://session.commonstatesaver.com/index.php';
 // $handler = new ApiSessionHandler($apiBaseUrl);
-require 'RedisSessionHandler.php';
 
-$redisHost = 'clustercfg.pgsession2.pr3z0i.memorydb.ap-south-1.amazonaws.com';
-$redisPort = 6379;
+// require_once 'RedisSessionHandler.php';
+// $redisHost = 'clustercfg.pgsession2.pr3z0i.memorydb.ap-south-1.amazonaws.com';
+// $redisPort = 6379;
+// $ttl = 3600;
+// $handler = new RedisSessionHandler($redisHost, $redisPort, $ttl);
+
+
+require_once 'RedisClusterSessionHandler.php';
+$clusterNodes = [
+    'clustercfg.pgsession2.pr3z0i.memorydb.ap-south-1.amazonaws.com:6379'
+];
 $ttl = 3600;
-$handler = new RedisSessionHandler($redisHost, $redisPort, $ttl);
+$handler = new RedisClusterSessionHandler($clusterNodes, $ttl);
 
 session_set_save_handler($handler, true);
 session_start();
